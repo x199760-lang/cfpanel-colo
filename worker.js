4861812433f978f467bfdba39500b5f40de5236a
x199760-lang <hux199760@gmail.com>
2026-06-15 11:45:53 -0700

    更新 worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,35 @@
 export default {
   async fetch(request) {
-    return Response.json({
+    const cf = request.cf || {};
+
+    const data = {
+      ok: true,
       ip: request.headers.get("CF-Connecting-IP") || "",
-      colo: request.cf?.colo || "",
-      city: request.cf?.city || "",
-      country: request.cf?.country || "",
-      asn: request.cf?.asn || "",
-      isp: request.cf?.asOrganization || "",
+      colo: cf.colo || "",
+      city: cf.city || "",
+      country: cf.country || "",
+      continent: cf.continent || "",
+      region: cf.region || "",
+      regionCode: cf.regionCode || "",
+      latitude: cf.latitude || "",
+      longitude: cf.longitude || "",
+      asn: cf.asn || "",
+      asOrganization: cf.asOrganization || "",
+      timezone: cf.timezone || "",
+      clientTcpRtt: cf.clientTcpRtt || "",
+      httpProtocol: cf.httpProtocol || "",
+      tlsVersion: cf.tlsVersion || "",
+      tlsCipher: cf.tlsCipher || "",
       userAgent: request.headers.get("User-Agent") || "",
-      ray: request.headers.get("CF-Ray") || "",
+      cfRay: request.headers.get("CF-Ray") || "",
       time: new Date().toISOString()
+    };
+
+    return new Response(JSON.stringify(data, null, 2), {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*"
+      }
     });
   }
 }
